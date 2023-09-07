82917fd7119fea5ea2eac3913eb51efeaebd194e
YashSingh08 <yashsingh9557090791@gmail.com>
2023-09-08 00:32:19 +0530

    uploading files

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {
+  AboutUs,
+  Chef,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
+import FindUs from "./container/Findus/FindUs";
+import "./App.css";
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+
+export default App;
